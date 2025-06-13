4e535f3bf89fbe80a729c2113b21913c636dc2c9
bottraker <jjimportado60@gmail.com>
2025-06-13 19:55:25 -0300

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+repositories {
+    google()
+    mavenCentral()
+}
+android {
+    namespace = "com.example.miapp"
+    compileSdk = 33
+    defaultConfig {
+        applicationId = "com.example.miapp"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+dependencies {
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+}
